df675c470c618043edc5f42886e97c7cb02d154a
Ferran Castañé <ferrancastane11@gmail.com>
2019-03-22 15:28:32 +0100

    Commit 2

--- peupagina/peupagina.php
+++ peupagina/peupagina.php
@@ -8,9 +8,18 @@ Author: El teu nom i cognom
 Author URI: L'adreça URL del teu compte de Github
 License: GNU GPLv3
 */
-function funcio_peupagina($art, $text){
+function funcio_peupagina($atr,$text) {
 	return '<a>Peu de pàgina</a>';
 }
+
 add_shortcode('peupagina', 'funcio_peupagina');
 
+function admin_menu_peu_pagina(){
+	$titolPagina="Peu de pàgina per defecte";
+	$titolEntrada="Peu de pàgina per defecte";
+	$role="administrator";
+	$id="peu_pagina_per_defecte";
+	add_menu_page($titolPagina,$titolEntrada,"read",$role,$id); ;
+	}
+	add_action('admin_menu','admin_menu_peu_pagina');
 ?>
